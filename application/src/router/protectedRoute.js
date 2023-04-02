66eba8b2ca50ed0927c873b92d99424f7309f42a
Spencer Alexander <slalexander92@gmail.com>
2023-04-02 12:04:44 -0700

    refactor(protectedRoute): make `protectedRoute` react-router-dom v6 compatible

--- application/src/router/protectedRoute.js
+++ application/src/router/protectedRoute.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 
-export const ProtectedRoute = ({component: Component, ...rest}) => {
+export const ProtectedRoute = ({ children }) => {
   const auth = useSelector(state => state.auth);
   const isAuthenticated = !!auth && !!auth.email && !!auth.token;
 
-  return (
-    <Route {...rest}
-      render={props => {
-        if (isAuthenticated) {
-          return <Component {...props} />
-        } else {
-          return <Redirect to={
-            {
-              pathname: '/login',
-              state: {
-                from: props.location,
-              }
-            }
-          } />
-        }
-      }}
-    />
-  )
-}
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
 
+  return children;
+};
