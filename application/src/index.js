03b3b11e8c0f8f25bf2542f729a4ea07b88b8cef
Spencer Alexander <slalexander92@gmail.com>
2023-04-02 15:45:26 -0700

    chore(index): update syntax to mount app

--- application/src/index.js
+++ application/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootElement = document.getElementById('root');
+const root = ReactDOMClient.createRoot(rootElement);
+
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
