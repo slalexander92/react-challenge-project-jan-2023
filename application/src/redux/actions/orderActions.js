b22f82a6fd60b481cbc7da37c5bc8a3c11c7505f
Spencer Alexander <slalexander92@gmail.com>
2023-04-05 18:29:07 -0700

    feat(orderActions): add `orderActions`

---
+++ application/src/redux/actions/orderActions.js
@@ -0,0 +1,24 @@
+import { GET_ORDERS } from './types';
+import { requestHandler } from '../../services/request-handler.service';
+
+export const setOrders = orderList => {
+  return {
+    type: GET_ORDERS,
+    payload: { orderList },
+  }
+}
+
+export const getOrders = () => {
+  return dispatch => {
+    return requestHandler.makeRequest('GET', 'current-orders')
+      .then(({ success, orders }) => {
+        if (!success) {
+            console.log('Error getting orders');
+
+            return;
+        }
+
+        dispatch(setOrders(orders));
+      });
+  }
+}
