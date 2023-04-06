0433787161cae6ff051f7a1e54178fa09936411f
Spencer Alexander <slalexander92@gmail.com>
2023-04-06 14:14:44 -0700

    feat(user.model): export userModal

---
+++ server/models/user.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+}, {
+  timestamps: true,
+  collection: 'Users',
+});
+
+module.exports = mongoose.model('User', UserSchema);
